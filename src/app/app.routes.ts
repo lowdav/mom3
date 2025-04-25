38f507d6c2a0a70aa4f4a07f1a92c32c9102bc5d
David Löwy <david.lowy@hotmail.com>
2025-04-25 16:16:45 +0200

    Grundstruktur + styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'convert', component: ConvertComponent},
+    { path: 'about', component: AboutComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', component: NotfoundComponent }
+];
