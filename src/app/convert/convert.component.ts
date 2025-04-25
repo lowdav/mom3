c702814a29d458d32a9b6b6b670895e9930a0cbf
David Löwy <david.lowy@hotmail.com>
2025-04-25 19:59:14 +0200

    konvertering påbörjad

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+  num1: number = 0;
+  result: number = 0;
+  temptype: 'C' | 'F' = 'C';
+  othertype: string = "Fahrenheit";
+
+
+ConvertTemp(): void {
+  if (this.temptype === 'C') {
+    this.result = Math.round(this.num1 * 9 / 5) + 32;
+  } else {
+    this.result = Math.round((this.num1 - 32) * 5 / 9);
+  }
+}
+
+printUnit(): void {
+  this.result = 0;
+  if (this.temptype === 'C') {
+    this.othertype = "Fahrenheit";
+  } else {
+    this.othertype = "Celsius"; 
+  }
+}
 
 }
+
+
+
